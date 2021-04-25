13cb1acad9709956bec47068757427cbb5a1881c
Gx <417987689@QQ.COM>
2021-04-25 23:42:02 +0800

    ~注释

--- plugin_common/src/main/java/com/sn/plugin_common/ui/TestEditText.java
+++ plugin_common/src/main/java/com/sn/plugin_common/ui/TestEditText.java
@@ -2,18 +2,22 @@ package com.sn.plugin_common.ui;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.EditText;
 
 public class TestEditText extends EditText {
     public TestEditText(Context context) {
         super(context);
+        Log.e("TestEditText", "context : " + context.toString());
     }
 
     public TestEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
+        Log.e("TestEditText", "AttributeCount : " + attrs.getAttributeCount());
     }
 
     public TestEditText(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        Log.e("TestEditText", "defStyleAttr : " + defStyleAttr);
     }
 }

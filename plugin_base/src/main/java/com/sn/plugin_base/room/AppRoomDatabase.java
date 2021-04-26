e8b2868d4631dcf2d968237382233f15c49443d7
guoxu <guoxu@hualai.com>
2021-04-26 10:01:48 +0800

    ～

--- plugin_base/src/main/java/com/sn/plugin_base/room/AppRoomDatabase.java
+++ plugin_base/src/main/java/com/sn/plugin_base/room/AppRoomDatabase.java
@@ -6,12 +6,6 @@ import androidx.room.RoomDatabase;
 import com.sn.plugin_base.data.UserInfo;
 
 
-/**
- * @Copyright 天津华来科技股份有限公司北京分公司
- * @Author
- * @Date 2021/3/5 10:14
- * Describe
- */
 //@Database(entities = {UserInfo.class}, version = 1)
 public abstract class AppRoomDatabase extends RoomDatabase {
 

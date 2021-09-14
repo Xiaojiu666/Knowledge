772c4e9adbec0d09eb2509e15b1f32f32d2c21f3
guoxu <guoxu@hualai.com>
2021-09-14 19:28:40 +0800

    [Dagger2] 从新梳理@Injcet

---
+++ module_task/src/main/java/com/gx/task/di/demo/CPU.kt
@@ -0,0 +1,8 @@
+package com.gx.task.di.demo
+
+import javax.inject.Inject
+
+class CPU {
+
+    fun getCpuName() = "I9-10900X"
+}
--- module_task/src/main/java/com/gx/task/di/demo/Computer.kt
+++ module_task/src/main/java/com/gx/task/di/demo/Computer.kt
@@ -2,6 +2,6 @@ package com.gx.task.di.demo
 
 import javax.inject.Inject
 
-class Computer @Inject constructor() {
+class Computer @Inject constructor(var cpu: CPU) {
     var phoneName = "Mac"
 }

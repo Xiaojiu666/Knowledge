6125028529b466f06d418429091ab8f8f17f56da
Xiaojiu666 <417987689@qq,com>
2023-01-29 15:35:48 +0800

    + [DA] 二叉搜索树学习

--- DataAlgorithm/Algorithm/app/src/main/java/com/study/algorithm/DataStructure/Tree/Search/BinaryTreeOpts.java
+++ DataAlgorithm/Algorithm/app/src/main/java/com/study/algorithm/DataStructure/Tree/Search/BinaryTreeOpts.java
@@ -4,19 +4,14 @@ import com.study.algorithm.DataStructure.Tree.Traversal.TreeNode;
 
 public class BinaryTreeOpts {
 
-
-    public static void main(String[] args) {
-        int[] datas = new int[]{100, 80, 200, 54, 90, 150, 210};
-    }
-
     static BinarySearchTreeNode root;
 
-    private void insert(int data) {
+
+    private static void insert(int data) {
         if (root == null) {
-            root = new BinarySearchTreeNode();
+            root = new BinarySearchTreeNode(data);
             return;
         }
-
         BinarySearchTreeNode node = root;
         while (node != null) {
             if (data < node.getData()) {
@@ -35,9 +30,101 @@ public class BinaryTreeOpts {
                 node = node.getRightNode();
             }
         }
+    }
 
 
-//        if (root.getData())
+    public static void main(String[] args) {
+//        int[] datas = new int[]{100, 80, 200, 54, 90, 150, 210};
+        int[] datas = new int[]{60, 40, 80, 30, 50, 70, 100, 45, 55};
+        for (int i = 0; i < datas.length; i++) {
+            insert(datas[i]);
+        }
+        mid(root);
+//        find(100);
+        System.out.println(" ");
+        remove(40);
+        mid(root);
+    }
+
+    private static void remove(int data) {
+        BinarySearchTreeNode node = root;
+        BinarySearchTreeNode parent = null;
+        // 1. 优先找到要删除的节点 和 他的父节点
+        while (node != null && node.getData() != data) {
+            parent = node;
+            if (data > node.getData()) {
+                node = node.getRightNode();
+            } else if (data < node.getData()) {
+                node = node.getLeftNode();
+            }
+        }
+
+        if (node == null) {
+            return;
+        }
+
+        // 4.  在Node只有两个子节点时
+        // 4.1 需要找到最对的
+        if (node.getLeftNode() != null && node.getRightNode() != null) {
+            BinarySearchTreeNode minNode = node.getRightNode();
+            BinarySearchTreeNode minParentNode = node;
+            while (minNode.getLeftNode() != null) {
+                minParentNode = minNode;
+                minNode = minNode.getLeftNode();
+
+            }
+            node.setData(minNode.getData());
+            node = minNode;
+            parent = minParentNode;
+            System.out.println("要被删除得节点有两个子节点 ");
+        }
+
+
+        //2.  找到要被删除节点的 的 子左右节点，
+        //2.1 在Node只有单子节点时 ， 删除的根本在于，将Node父节点的指针，指向Node的子节点
+        BinarySearchTreeNode childNode = null;
+        if (node.getLeftNode() != null) {
+            childNode = node.getLeftNode();
+        } else if (node.getRightNode() != null) {
+            childNode = node.getRightNode();
+        }
+
+        //3.   重新对父节点赋值
+        if (parent == null) {
+            root = childNode;
+        } else if (parent.getLeftNode() == node) {
+            parent.setLeftNode(childNode);
+        } else if (parent.getRightNode() == node) {
+            parent.setRightNode(childNode);
+        }
+
+
+    }
+
+    private static void find(int data) {
+        BinarySearchTreeNode node = root;
+        while (node != null) {
+            if (node.getData() < data) {
+                node = node.getLeftNode();
+            } else if (node.getData() > data) {
+                node = node.getRightNode();
+            } else {
+                System.out.println("找到了 ： " + data);
+                return;
+            }
+        }
+        System.out.println("没找到 ： " + data);
+
+    }
+
+
+    private static void mid(BinarySearchTreeNode cur) {
+        if (cur == null) {
+            return;
+        }
+        mid(cur.getLeftNode());
+        System.out.print(" ," + cur.getData());
+        mid(cur.getRightNode());
 
 
     }
